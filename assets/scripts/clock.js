4c2179b92d7e1b3919c5bfd96fd8669e58f0f238
Damon Holland <damonrholland@gmail.com>
2020-09-29 12:02:24 -0700

    More Code Cleaning

--- assets/scripts/clock.js
+++ assets/scripts/clock.js
@@ -1,16 +1,24 @@
 function clock2D(canvasID){
   const CENTER_RADIUS = 5;
-  const NUM_TICKS = 60;
   const CLOCK_COLOR = "black";
   const CLOCK_BG_COLOR = "transparent";
   const HAND_COLOR = "teal";
   const HAND_OVERLAP = 20;
-  const HOUR_HAND_WIDTH = 5, MINUTE_HAND_WIDTH = 4, SECOND_HAND_WIDTH = 1;
-  const HOUR_HAND_RATIO = 0.5, LONG_HAND_CUTOFF = 25;
-  const LONG_TICK_INTERVAL = 5, LONG_TICK_LENGTH = 20, LONG_TICK_WIDTH = 2;
-  const SHORT_TICK_LENGTH = 10, SHORT_TICK_WIDTH = 1;
+  const HOUR_HAND_WIDTH = 5;
+  const MINUTE_HAND_WIDTH = 4;
+  const SECOND_HAND_WIDTH = 1;
+  const HOUR_HAND_RATIO = 0.5;
+  const LONG_HAND_CUTOFF = 25;
+  const NUM_TICKS = 60;
+  const LONG_TICK_INTERVAL = 5;
+  const LONG_TICK_LENGTH = 20;
+  const SHORT_TICK_LENGTH = 10;
+  const LONG_TICK_WIDTH = 2;
+  const SHORT_TICK_WIDTH = 1;
   const CIRCLE_DEGREES = 360;
-  const HOURS_IN_ANALOG_DAY = 12, MINUTES_IN_HOUR = 60, SECONDS_IN_MINUTE = 60;
+  const HOURS_IN_ANALOG_DAY = 12;
+  const MINUTES_IN_HOUR = 60;
+  const SECONDS_IN_MINUTE = 60;
   var canvas = document.getElementById(canvasID);
   var context = canvas.getContext("2d");
   var origin = canvas.width / 2;
@@ -19,27 +27,35 @@ function clock2D(canvasID){
   var minute = date.getMinutes();
   var second = date.getSeconds();
 
-  //Draw The Empty Clock
   context.clearRect(0, 0, canvas.width, canvas.height);
-  drawCircle(context, origin, origin, CLOCK_COLOR, CLOCK_BG_COLOR, SECOND_HAND_WIDTH);
+  drawCircle(context, origin, origin, CLOCK_COLOR, CLOCK_BG_COLOR,
+             SECOND_HAND_WIDTH);
   context.save();
   for (var i = 1; i <= NUM_TICKS; i++){
     rotateContext(context, origin, (CIRCLE_DEGREES / NUM_TICKS));
     context.beginPath();
     context.moveTo(origin, 0);
-    context.lineWidth = (i % LONG_TICK_INTERVAL == 0) ? LONG_TICK_WIDTH : SHORT_TICK_WIDTH;
-    tickLength = (i % LONG_TICK_INTERVAL == 0) ? LONG_TICK_LENGTH : SHORT_TICK_LENGTH;
+    context.lineWidth = (i % LONG_TICK_INTERVAL == 0) ? LONG_TICK_WIDTH :
+                                                        SHORT_TICK_WIDTH;
+    tickLength = (i % LONG_TICK_INTERVAL == 0) ? LONG_TICK_LENGTH :
+                                                 SHORT_TICK_LENGTH;
     context.lineTo(origin, tickLength);
     context.stroke();
   }
   context.restore();
 
-  drawHand(context, origin, HAND_COLOR, HOUR_HAND_WIDTH, HOUR_HAND_RATIO * origin, HAND_OVERLAP, ((CIRCLE_DEGREES / HOURS_IN_ANALOG_DAY) * hour));
-  drawHand(context, origin, HAND_COLOR, MINUTE_HAND_WIDTH, origin - LONG_HAND_CUTOFF, HAND_OVERLAP, ((CIRCLE_DEGREES / MINUTES_IN_HOUR) * minute));
-  drawHand(context, origin, CLOCK_COLOR, SECOND_HAND_WIDTH, origin - LONG_HAND_CUTOFF, HAND_OVERLAP, (CIRCLE_DEGREES / SECONDS_IN_MINUTE) * second);
+  drawHand(context, origin, HAND_COLOR, HOUR_HAND_WIDTH,
+           HOUR_HAND_RATIO * origin, HAND_OVERLAP,
+           ((CIRCLE_DEGREES / HOURS_IN_ANALOG_DAY) * hour));
+  drawHand(context, origin, HAND_COLOR, MINUTE_HAND_WIDTH,
+           origin - LONG_HAND_CUTOFF, HAND_OVERLAP,
+           ((CIRCLE_DEGREES / MINUTES_IN_HOUR) * minute));
+  drawHand(context, origin, CLOCK_COLOR, SECOND_HAND_WIDTH,
+           origin - LONG_HAND_CUTOFF, HAND_OVERLAP,
+           (CIRCLE_DEGREES / SECONDS_IN_MINUTE) * second);
 
-  //Draw middle button
-  drawCircle(context, origin, CENTER_RADIUS, CLOCK_COLOR, CLOCK_COLOR, SECOND_HAND_WIDTH);
+  drawCircle(context, origin, CENTER_RADIUS, CLOCK_COLOR, CLOCK_COLOR,
+             SECOND_HAND_WIDTH);
 }
 
 function drawCircle(context, origin, radius, outlineColor, fillColor, penWidth){
@@ -51,7 +67,8 @@ function drawCircle(context, origin, radius, outlineColor, fillColor, penWidth){
   context.stroke();
 }
 
-function drawHand(context, origin, handColor, handWidth, handLength, handOverlap, rotationDegrees){
+function drawHand(context, origin, handColor, handWidth, handLength,
+                  handOverlap, rotationDegrees){
   context.save();
   setContextAttributes(context, handColor, handColor, handWidth);
   rotateContext(context, origin, rotationDegrees);
